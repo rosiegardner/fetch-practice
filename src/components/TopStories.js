b38a0e955127ba14ca520525dde85a2859f641b3
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-10 11:53:31 -0700

    set useEffect hook for api

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function TopStories() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [topStories, setTopStories] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+      .then(response => response.json())
+      .then((jsonifiedResponse) => {
+        setTopStories(jsonifiedResponse.results)
+        setIsLoaded(true)
+      })
+      .catch((error) => {
+        setError(error)
+        setIsLoaded(true)
+      });
+  }, [])
 }
 
 export default TopStories;
