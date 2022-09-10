7ad612af868a8d0f48de9fe936e2eb33e8ebdaa9
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-10 11:46:51 -0700

    create file components - add local state

---
+++ src/components/TopStories.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function TopStories() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [topStories, setTopStories] = useState([]);
+}
+
+export default TopStories;
