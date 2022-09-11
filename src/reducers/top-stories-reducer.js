af90ee53ffeff4158f913aa2f349f3c6cc7b1b9e
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-11 14:51:13 -0700

    passes test

--- src/reducers/top-stories-reducer.js
+++ src/reducers/top-stories-reducer.js
@@ -1,5 +1,13 @@
+import * as c from '../actions/ActionTypes';
+
 const topStoriesReducer = (state, action) => {
   switch (action.type) {
+    case c.GET_TOP_STORIES_SUCCESS:
+      return {
+        ...state, 
+        isLoaded: true,
+        topStories: action.topStories
+      };
     default:
       throw new Error(`There is no action matching ${action.type}.`);
   }
