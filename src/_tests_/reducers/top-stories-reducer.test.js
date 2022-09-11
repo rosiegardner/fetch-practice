7f3c45f2570c5ffd1451aa8966d993861a02dadf
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-11 12:08:35 -0700

    add second and third test - expect pass

--- src/_tests_/reducers/top-stories-reducer.test.js
+++ src/_tests_/reducers/top-stories-reducer.test.js
@@ -1,6 +1,9 @@
 import topStoriesReducer from '../../reducers/top-stories-reducer';
+import * as c from './../../actions/ActionTypes';
 
 describe('topStoriesReducer', () => {
+
+  let action;
 
   const initialState = {
     isLoaded: false,
@@ -14,5 +17,31 @@ describe('topStoriesReducer', () => {
         topStoriesReducer(initialState, {type: null})
       }
     ).toThrowError("There is no action matching null.");
+  });
+
+  test('requesting topStories should successfully change isLoaded from false to true', () => {
+    action = {
+      type: c.REQUEST_TOP_STORIES
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      topStories: [],
+      error: null
+    });
+  });
+
+  test('successfully getting top stories should change isLoaded to true and update topStories', () => {
+    const topStories = "An article";
+    action = {
+      type: c.GET_TOP_STORIES_SUCCESS,
+      topStories 
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      topStories: "An article",
+      error: null 
+    });
   });
 });
